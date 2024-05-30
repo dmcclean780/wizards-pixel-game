973b97fd05a774d7045e47b19b78d5449e33d086
Daniel <dmcclean780@gmail.com>
2024-05-30 16:17:00 +0100

    Tided Movable Solid Code

--- public/FallingSand/Elements/Solids/MovableSolids/MovableSolid.js
+++ public/FallingSand/Elements/Solids/MovableSolids/MovableSolid.js
@@ -2,110 +2,156 @@ import { Empty } from "../../Empty.js";
 import { Liquid } from "../../Liquids/Liquid.js";
 import { Solid } from "../Solid.js";
 
-class MovableSolid extends Solid{
+class MovableSolid extends Solid {
     inertialResistance;
 
     move(i, gameArray, canvasData, newGameArray, updatedPositions){
+
         var velocity = this.getAlpha(gameArray, i)
-        var belowElement = this.getNeighbourElement(gameArray, i+canvasData.width)
-        if(this.density>belowElement.density){
-            for(var j=0; j<velocity; j++){
-                var belowElement = this.getNeighbourElement(gameArray, i+canvasData.width)
-                if(this.density>belowElement.density && !(belowElement instanceof Solid) && updatedPositions.includes(i+canvasData.width)==false && i+canvasData.width<canvasData.width*canvasData.height){
-                    newGameArray=this.swapPositions(newGameArray, updatedPositions, i, i+canvasData.width)
-                    i=i+canvasData.width
+
+        var moveDownPossible=this.moveDown(i, gameArray, canvasData, newGameArray, updatedPositions, velocity);
+        if(!Number.isInteger(moveDownPossible)){
+            return moveDownPossible;
+        }
+
+        var dir = Math.random() < 0.5;
+        
+        if (dir) {
+
+            var moveHorizontalRightPossible=this.moveHorizontalRight(i, canvasData, newGameArray, updatedPositions, velocity)
+            if(!Number.isInteger(moveHorizontalRightPossible)){
+                return moveHorizontalRightPossible
+            }
+
+            var moveDownDiagonalRightPossible=this.moveDownDiagonalRight(i, gameArray, canvasData, newGameArray, updatedPositions)
+            if(!Number.isInteger(moveDownDiagonalRightPossible)){
+                return moveDownDiagonalRightPossible
+            }
+    
+        }
+
+        if(!dir){
+            var moveHorizontalLeftPossible=this.moveHorizontalLeft(i, canvasData, newGameArray, updatedPositions, velocity)
+            if(!Number.isInteger(moveHorizontalLeftPossible)){
+                return moveHorizontalLeftPossible
+            }
+    
+            var moveDownDiagonalLeftPossible=this.moveDownDiagonalLeft(i, gameArray, canvasData, newGameArray, updatedPositions)
+            if(!Number.isInteger(moveDownDiagonalLeftPossible)){
+                return moveDownDiagonalLeftPossible
+            }
+        }
+
+        return newGameArray;
+
+    }
+
+    moveDown(i, gameArray, canvasData, newGameArray, updatedPositions, velocity) {
+        var belowElement = this.getNeighbourElement(gameArray, i + canvasData.width)
+        if (this.density > belowElement.density) {
+            for (var j = 0; j < velocity; j++) {
+                var belowElement = this.getNeighbourElement(gameArray, i + canvasData.width)
+                if (this.density > belowElement.density && !(belowElement instanceof Solid) && updatedPositions.includes(i + canvasData.width) == false && i + canvasData.width < canvasData.width * canvasData.height) {
+                    newGameArray = this.swapPositions(newGameArray, updatedPositions, i, i + canvasData.width)
+                    i = i + canvasData.width
                 }
-                else{
-                    velocity=9-velocity;
-                    newGameArray=this.updateAlphaByte(newGameArray, velocity, i);
+                else {
+                    velocity = 9 - velocity;
+                    newGameArray = this.updateAlphaByte(newGameArray, velocity, i);
                     return newGameArray
                 }
             }
-            if(velocity<this.terminalVelocity){
+            if (velocity < this.terminalVelocity) {
                 velocity++;
             }
-            newGameArray =this.updateAlphaByte(newGameArray, velocity, i)
+            newGameArray = this.updateAlphaByte(newGameArray, velocity, i)
             return newGameArray;
         }
+        return -1;
+    }
 
-        var dir=Math.random() < 0.5;
-        if(dir){
-            
-            if(velocity>0){
-                for(var j=0; j<velocity; j++){
-                    var adjacentElement = this.getNeighbourElement(newGameArray, i+1);
-                    if(this.density>adjacentElement.density && updatedPositions.includes(i+1)==false && i%canvasData.width!=0){
-                        newGameArray=this.swapPositions(newGameArray, updatedPositions, i, i+1)
-                        i=i+1
-                    }
-                    else{
-                        var FrictionChance=Math.random();
-                        if(FrictionChance>1-this.inertialResistance){
-                            velocity --
-                        }
-                        newGameArray =this.updateAlphaByte(newGameArray, velocity, i)
-                        return newGameArray
-                    }
-                }
-                var FrictionChance=Math.random()
-                if(FrictionChance>1-this.inertialResistance){
-                    velocity --
-                }
-                newGameArray =this.updateAlphaByte(newGameArray, velocity, i)
-                
-            }
-
-            if(i+canvasData.width+1<canvasData.width*canvasData.height && updatedPositions.indexOf(i+canvasData.width+1)==-1 && i%canvasData.width!=canvasData.width-1){
-                var adjacentElement = this.getNeighbourElement(gameArray, i+1);
-                if(this.density>adjacentElement.density){
-                    var destinationElement=this.getNeighbourElement(gameArray, i+canvasData.width+1);
-                    if(this.density>destinationElement.density && destinationElement instanceof Liquid || destinationElement instanceof Empty){
-                        newGameArray=this.swapPositions(newGameArray, updatedPositions, i, i+canvasData.width+1)
-                    }
+    moveDownDiagonalLeft(i, gameArray, canvasData, newGameArray, updatedPositions){
+        if (i + canvasData.width - 1 < canvasData.width * canvasData.height && updatedPositions.indexOf(i + canvasData.width - 1) == -1 && i % canvasData.width != 0) {
+            var adjacentElement = this.getNeighbourElement(gameArray, i - 1);
+            if (this.density > adjacentElement.density) {
+                var destinationElement = this.getNeighbourElement(gameArray, i + canvasData.width - 1);
+                if (this.density > destinationElement.density && destinationElement instanceof Liquid || destinationElement instanceof Empty) {
+                    newGameArray = this.swapPositions(newGameArray, updatedPositions, i, i + canvasData.width - 1)
+                    return newGameArray;
                 }
             }
-
-            return newGameArray
         }
-        
-        if(velocity>0){
-            for(var j=0; j<velocity; j++){
-                var adjacentElement = this.getNeighbourElement(newGameArray, i-1);
-                if(this.density>adjacentElement.density && updatedPositions.includes(i-1)==false && i%canvasData.width!=0){
-                    newGameArray=this.swapPositions(newGameArray, updatedPositions, i, i-1)
-                    i=i-1
+        return -1;
+    }
+
+    moveDownDiagonalRight(i, gameArray, canvasData, newGameArray, updatedPositions){
+        if (i + canvasData.width + 1 < canvasData.width * canvasData.height && updatedPositions.indexOf(i + canvasData.width + 1) == -1 && i % canvasData.width != canvasData.width - 1) {
+            var adjacentElement = this.getNeighbourElement(gameArray, i + 1);
+            if (this.density > adjacentElement.density) {
+                var destinationElement = this.getNeighbourElement(gameArray, i + canvasData.width + 1);
+                if (this.density > destinationElement.density && destinationElement instanceof Liquid || destinationElement instanceof Empty) {
+                    newGameArray = this.swapPositions(newGameArray, updatedPositions, i, i + canvasData.width + 1)
+                    return newGameArray;
                 }
-                else{
-                    var FrictionChance=Math.random()
-                    if(FrictionChance>1-this.inertialResistance){
-                        velocity --
+            }
+        }
+        return -1;
+    }
+
+    moveHorizontalLeft(i, canvasData, newGameArray, updatedPositions, velocity){
+        if (velocity > 0) {
+            for (var j = 0; j < velocity; j++) {
+                var adjacentElement = this.getNeighbourElement(newGameArray, i - 1);
+                if (this.density > adjacentElement.density && updatedPositions.includes(i - 1) == false && i % canvasData.width != 0) {
+                    newGameArray = this.swapPositions(newGameArray, updatedPositions, i, i - 1)
+                    i = i - 1
+                }
+                else {
+                    var FrictionChance = Math.random()
+                    if (FrictionChance > 1 - this.inertialResistance) {
+                        velocity--
                     }
-                    newGameArray =this.updateAlphaByte(newGameArray, velocity, i)
+                    newGameArray = this.updateAlphaByte(newGameArray, velocity, i)
                     return newGameArray
                 }
             }
-            var FrictionChance=Math.random()
-            if(FrictionChance>1-this.inertialResistance){
-                velocity --
+            var FrictionChance = Math.random()
+            if (FrictionChance > 1 - this.inertialResistance) {
+                velocity--
             }
-            newGameArray =this.updateAlphaByte(newGameArray, velocity, i)
-            
-        }
+            newGameArray = this.updateAlphaByte(newGameArray, velocity, i)
+            return newGameArray;
 
-        if(i+canvasData.width-1<canvasData.width*canvasData.height && updatedPositions.indexOf(i+canvasData.width-1)==-1 && i%canvasData.width!=0){
-            var adjacentElement=this.getNeighbourElement(gameArray, i-1);
-            if(this.density>adjacentElement.density){
-                var destinationElement=this.getNeighbourElement(gameArray, i+canvasData.width-1);
-                if(this.density>destinationElement.density && destinationElement instanceof Liquid || destinationElement instanceof Empty){
-                    newGameArray=this.swapPositions(newGameArray, updatedPositions, i, i+canvasData.width-1)
+        }
+        return -1;
+    }
+
+    moveHorizontalRight(i, canvasData, newGameArray, updatedPositions, velocity){
+        if (velocity > 0) {
+            for (var j = 0; j < velocity; j++) {
+                var adjacentElement = this.getNeighbourElement(newGameArray, i + 1);
+                if (this.density > adjacentElement.density && updatedPositions.includes(i + 1) == false && i % canvasData.width != 0) {
+                    newGameArray = this.swapPositions(newGameArray, updatedPositions, i, i + 1)
+                    i = i + 1
+                }
+                else {
+                    var FrictionChance = Math.random();
+                    if (FrictionChance > 1 - this.inertialResistance) {
+                        velocity--
+                    }
+                    newGameArray = this.updateAlphaByte(newGameArray, velocity, i)
+                    return newGameArray
                 }
             }
+            var FrictionChance = Math.random()
+            if (FrictionChance > 1 - this.inertialResistance) {
+                velocity--
+            }
+            newGameArray = this.updateAlphaByte(newGameArray, velocity, i)
+            return newGameArray
         }
-            
-        return newGameArray;
-        
-        
+        return -1
     }
 }
 
-export {MovableSolid}
+export { MovableSolid }
